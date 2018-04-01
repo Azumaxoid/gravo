3491da0d4aaed6c49151ea2d5586e06987dfe8c5
azumaxoid <azumax.android@gmail.com>
2018-04-01 19:56:41 +0900

    fix

--- julius-docker/start.sh
+++ julius-docker/start.sh
@@ -2,8 +2,8 @@ docker run \
   --name julius \
   --net=host \
   -t \
-  -v ../dictionaly:/dictionaly
-  -v ../dictation-kit-v4.4:/dictation-kit-v4.4
-  --device=/dev/dsp1:/dev/dsp:rw
-  --rm fabito/julius
+  -v /home/azumaxoid/develop/gravo/dictionaly:/dictionaly \
+  -v /home/azumaxoid/develop/dictation-kit-v4.4:/dictation-kit-v4.4 \
+  --device=/dev/dsp1:/dev/dsp:rw \
+  --rm fabito/julius \
   julius -C /dictionaly/word.jconf -charconv sjis utf8 -module
